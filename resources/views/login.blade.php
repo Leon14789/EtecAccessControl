1badb8dece09d3378bc69df6c1880b7c89d31e6a
Leon14789 <leoortizinho@gmail.com>
2025-02-10 18:13:02 -0300

    layout for login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>LOGIN</title>
+   <link rel="stylesheet" href="assets/css/login.css">
+</head>
+<body>
+    <div class="background">
+        <div class="shape"></div>
+        <div class="shape"></div>
+    </div>
+    <form >
+        <img class="img-login" src="/assets/images/Cabecalho.png" alt="logo CPS" srcset="">
+        <label for="username">Email</label>
+        <input type="text" placeholder="Insira  seu Email" id="username">
+
+        <label for="password">Senha</label>
+        <input type="password" placeholder="Insira sua Senha" id="password">
+
+        <button>ENTRAR</button>
+    </form>
+</body>
+</html>
